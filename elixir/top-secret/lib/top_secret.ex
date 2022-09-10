c68ba6c53fb41dfe996f5662388bd88be8038adc
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-10 10:13:10 +0200

    Elixir - top secret - iteration 1

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -1,13 +1,48 @@
 defmodule TopSecret do
-  def to_ast(string) do
-    # Please implement the to_ast/1 function
+  def to_ast(string), do: Code.string_to_quoted!(string)
+
+  def decode_secret_message_part({op, _meta, children} = ast, acc) do
+    if op in [:def, :defp] do
+      function_tuple = get_first_children_but_not_guard(children)
+
+      message_part =
+        make_message_part(
+          function_tuple |> get_op_from_tuple() |> Atom.to_string(),
+          function_tuple |> get_children_from_tuple()
+        )
+
+      {ast, [message_part | acc]}
+    else
+      {ast, acc}
+    end
   end
 
-  def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
-  end
+  def decode_secret_message_part(ast, acc), do: {ast, acc}
 
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    to_ast(string)
+    |> Macro.prewalk([], &decode_secret_message_part/2)
+    |> elem(1)
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+
+  # private functions
+
+  defp get_op_from_tuple({op, _meta, _children}), do: op
+
+  defp get_children_from_tuple({_op, _meta, children}), do: children
+
+  defp get_first_children_but_not_guard([{:when, _metadata, args} | _tail]) do
+    get_first_children_but_not_guard(args)
+  end
+
+  defp get_first_children_but_not_guard([head | _tail]), do: head
+
+  defp make_message_part(_function_name, []), do: ""
+  defp make_message_part(_function_name, nil), do: ""
+
+  defp make_message_part(function_name, args) do
+    String.slice(function_name, 0..(length(args) - 1))
   end
 end
