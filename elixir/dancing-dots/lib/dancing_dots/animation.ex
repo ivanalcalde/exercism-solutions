e0425d681dabb274b6fedd1352d0aa142381b822
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-12 12:58:34 +0200

    :bookmark: Elixir - dancing dots - iteration 1

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -4,13 +4,104 @@ defmodule DancingDots.Animation do
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts()) :: {:ok, opts()} | {:error, String.t()}
+  @callback handle_frame(dot(), frame_number(), opts()) :: dot()
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+
+      def init(opts) do
+        {:ok, opts}
+      end
+
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, _) when rem(frame_number, 4) == 0 do
+    %{dot | opacity: dot.opacity / 2}
+  end
+
+  def handle_frame(dot, _, _), do: dot
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @opts_validations [
+    {:velocity, validate_type: "number", validate_required: true}
+  ]
+
+  @impl DancingDots.Animation
+  def init(opts) do
+    case validate_opts(opts) do
+      {:ok} -> {:ok, opts}
+      {:error, [first_error | _t]} -> {:error, first_error}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, 1, _), do: dot
+
+  def handle_frame(dot, frame_number, velocity: velocity) do
+    radius_inc = (frame_number - 1) * velocity
+
+    %{dot | radius: dot.radius + radius_inc}
+  end
+
+  defp valid_type?("number", value), do: is_number(value)
+
+  defp validate_opt(
+         {field, [validate_type: type, validate_required: required?]},
+         {opts, errors} = acc
+       ) do
+    opt_value = Keyword.get(opts, field)
+
+    error_message = make_error_message(field, type, opt_value)
+
+    case opt_value do
+      nil ->
+        if(required?) do
+          {opts, [error_message | errors]}
+        else
+          acc
+        end
+
+      opt_value ->
+        if(valid_type?(type, opt_value)) do
+          acc
+        else
+          {opts, [error_message | errors]}
+        end
+    end
+  end
+
+  defp validate_opts(opts) do
+    {_opts, errors} =
+      @opts_validations
+      |> Enum.reduce({opts, []}, &validate_opt/2)
+
+    case errors do
+      [] -> {:ok}
+      errors -> {:error, errors}
+    end
+  end
+
+  defp make_error_message(field, type, nil) do
+    "The :#{field} option is required, and its value must be a #{type}. Got: nil"
+  end
+
+  defp make_error_message(field, type, value) when is_bitstring(value) do
+    "The :#{field} option is required, and its value must be a #{type}. Got: \"#{value}\""
+  end
+
+  defp make_error_message(field, type, value) do
+    "The :#{field} option is required, and its value must be a #{type}. Got: #{value}"
+  end
 end
