0a0fbe0d8cd52475f2eff900f13fd1584097f158
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-14 19:21:21 +0200

    [WIP] Elixir - resistor color
    ✅ `colors/0`

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -18,4 +18,14 @@ defmodule ResistorColor do
   @spec code(atom | String.t()) :: integer()
   def code(color) when is_bitstring(color), do: code(String.to_atom(color))
   def code(color) when is_atom(color), do: @band_colors[color]
+
+  @doc """
+  Return the list of the colors
+  """
+  @spec colors() :: [String.t()]
+  def colors() do
+    @band_colors
+    |> Keyword.keys()
+    |> Enum.map(&Atom.to_string/1)
+  end
 end
--- elixir/resistor-color/test/resistor_color_test.exs
+++ elixir/resistor-color/test/resistor_color_test.exs
@@ -1,62 +1,82 @@
 defmodule ResistorColorTest do
   use ExUnit.Case
 
-  # @tag :pending
-  test "returns black color code" do
-    assert ResistorColor.code(:black) == 0
-    assert ResistorColor.code("black") == 0
+  describe "color codes" do
+    # @tag :pending
+    test "returns black color code" do
+      assert ResistorColor.code(:black) == 0
+      assert ResistorColor.code("black") == 0
+    end
+
+    # @tag :pending
+    test "returns brown color code" do
+      assert ResistorColor.code(:brown) == 1
+      assert ResistorColor.code("brown") == 1
+    end
+
+    # @tag :pending
+    test "returns red color code" do
+      assert ResistorColor.code(:red) == 2
+      assert ResistorColor.code("red") == 2
+    end
+
+    # @tag :pending
+    test "returns orange color code" do
+      assert ResistorColor.code(:orange) == 3
+      assert ResistorColor.code("orange") == 3
+    end
+
+    # @tag :pending
+    test "returns yellow color code" do
+      assert ResistorColor.code(:yellow) == 4
+      assert ResistorColor.code("yellow") == 4
+    end
+
+    # @tag :pending
+    test "returns green color code" do
+      assert ResistorColor.code(:green) == 5
+      assert ResistorColor.code("green") == 5
+    end
+
+    # @tag :pending
+    test "returns blue color code" do
+      assert ResistorColor.code(:blue) == 6
+      assert ResistorColor.code("blue") == 6
+    end
+
+    # @tag :pending
+    test "returns violet color code" do
+      assert ResistorColor.code(:violet) == 7
+      assert ResistorColor.code("violet") == 7
+    end
+
+    # @tag :pending
+    test "returns grey color code" do
+      assert ResistorColor.code(:grey) == 8
+      assert ResistorColor.code("grey") == 8
+    end
+
+    # @tag :pending
+    test "returns white color code" do
+      assert ResistorColor.code(:white) == 9
+    end
   end
 
-  # @tag :pending
-  test "returns brown color code" do
-    assert ResistorColor.code(:brown) == 1
-    assert ResistorColor.code("brown") == 1
-  end
-
-  # @tag :pending
-  test "returns red color code" do
-    assert ResistorColor.code(:red) == 2
-    assert ResistorColor.code("red") == 2
-  end
-
-  # @tag :pending
-  test "returns orange color code" do
-    assert ResistorColor.code(:orange) == 3
-    assert ResistorColor.code("orange") == 3
-  end
-
-  # @tag :pending
-  test "returns yellow color code" do
-    assert ResistorColor.code(:yellow) == 4
-    assert ResistorColor.code("yellow") == 4
-  end
-
-  # @tag :pending
-  test "returns green color code" do
-    assert ResistorColor.code(:green) == 5
-    assert ResistorColor.code("green") == 5
-  end
-
-  # @tag :pending
-  test "returns blue color code" do
-    assert ResistorColor.code(:blue) == 6
-    assert ResistorColor.code("blue") == 6
-  end
-
-  # @tag :pending
-  test "returns violet color code" do
-    assert ResistorColor.code(:violet) == 7
-    assert ResistorColor.code("violet") == 7
-  end
-
-  # @tag :pending
-  test "returns grey color code" do
-    assert ResistorColor.code(:grey) == 8
-    assert ResistorColor.code("grey") == 8
-  end
-
-  # @tag :pending
-  test "returns white color code" do
-    assert ResistorColor.code(:white) == 9
+  describe "list of colors" do
+    # @tag :pending
+    test "returns the list of colors" do
+      assert ResistorColor.colors() == [
+               "black",
+               "brown",
+               "red",
+               "orange",
+               "yellow",
+               "green",
+               "blue",
+               "violet",
+               "grey",
+               "white"
+             ]
+    end
   end
 end
