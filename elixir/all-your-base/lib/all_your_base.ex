5c41e42cdfc1dd0ca31de9bf32d32928fc3e3baa
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-19 23:46:06 +0200

    :white_check_mark: Elixir - all your base - iteration 1

--- elixir/all-your-base/lib/all_your_base.ex
+++ elixir/all-your-base/lib/all_your_base.ex
@@ -5,6 +5,56 @@ defmodule AllYourBase do
   """
 
   @spec convert(list, integer, integer) :: {:ok, list} | {:error, String.t()}
+  def convert(_, _, output_base) when output_base < 2 do
+    {:error, "output base must be >= 2"}
+  end
+
+  def convert(_, input_base, _) when input_base < 2 do
+    {:error, "input base must be >= 2"}
+  end
+
   def convert(digits, input_base, output_base) do
+    if(Enum.all?(digits, fn digit -> digit in 0..(input_base - 1) end)) do
+      result =
+        digits
+        |> digits_to_decimal_number(input_base)
+        |> decimal_number_to_base(output_base)
+
+      {:ok, result}
+    else
+      {:error, "all digits must be >= 0 and < input base"}
+    end
+  end
+
+  defp digits_to_decimal_number(digits, base) do
+    exponent = length(digits) - 1
+    acc = {0, exponent}
+
+    {number, _} =
+      digits
+      |> Enum.reduce(acc, fn digit, {total, exponent} ->
+        {total + digit * Integer.pow(base, exponent), exponent - 1}
+      end)
+
+    number
+  end
+
+  defp decimal_number_to_base(number, base, digits \\ []) do
+    {quotient, remainder} = get_quotient_and_remainder(number, base)
+
+    digits = [remainder | digits]
+
+    if quotient == 0 do
+      digits
+    else
+      decimal_number_to_base(quotient, base, digits)
+    end
+  end
+
+  defp get_quotient_and_remainder(dividend, divisor) do
+    quotient = div(dividend, divisor)
+    remainder = rem(dividend, divisor)
+
+    {quotient, remainder}
   end
 end
