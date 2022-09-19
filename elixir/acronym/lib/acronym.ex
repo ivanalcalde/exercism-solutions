3f9ca845ffd55497cd3ba2435f60b9dca40556fe
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-19 15:30:33 +0200

    :white_check_mark: Elixir - acronym - iteration 1

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -5,5 +5,11 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.replace(~r/[-_]/, " ")
+    |> String.split()
+    |> Enum.map(&String.first/1)
+    |> Enum.map(&String.upcase/1)
+    |> Enum.join()
   end
 end
