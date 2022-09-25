65670fbff075e883236770194941e6589274cb11
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-25 08:07:40 +0200

    :white_check_mark: Elixir - anagram - iteration 1

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,26 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, &is_anagram_of(base, &1))
+  end
+
+  defp is_anagram_of(target, word) do
+    not is_same_word(target, word) &&
+      normalize(target) == normalize(word)
+  end
+
+  defp is_same_word(w1, w2) do
+    String.downcase(w1) == String.downcase(w2)
+  end
+
+  defp normalize(word) do
+    word |> String.downcase() |> sorted_word()
+  end
+
+  defp sorted_word(word) do
+    word
+    |> String.graphemes()
+    |> Enum.sort()
+    |> Enum.join()
   end
 end
