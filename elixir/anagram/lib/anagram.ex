517ff5a5bd3b4df023fa62c02274f812e1babf42
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-25 08:12:18 +0200

    :recycle: Elixir - anagram - iteration 2
    Update function names that return a boolean with the standard question mark at the end.

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,15 +4,15 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
-    Enum.filter(candidates, &is_anagram_of(base, &1))
+    Enum.filter(candidates, &anagram_of?(base, &1))
   end
 
-  defp is_anagram_of(target, word) do
-    not is_same_word(target, word) &&
+  defp anagram_of?(target, word) do
+    not same_word?(target, word) &&
       normalize(target) == normalize(word)
   end
 
-  defp is_same_word(w1, w2) do
+  defp same_word?(w1, w2) do
     String.downcase(w1) == String.downcase(w2)
   end
 
