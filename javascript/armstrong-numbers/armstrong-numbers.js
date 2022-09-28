b0e66e6a438da9484f4e34f5bec38766cb3521be
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-28 07:41:20 +0200

    :white_check_mark: JavaScript - Armstrong numbers

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,3 +1,19 @@
-export function isArmstrongNumber(number) {
+const sumList = xs => xs.reduce((acc, x) => acc + x, 0)
 
+const powList = exponent => (
+  xs => xs.map(x => Math.pow(x, exponent))
+)
+
+const digits = number => (
+  String(number)
+  .split('')
+  .map(Number)
+)
+
+export function isArmstrongNumber(number) {
+  const exponent = String(number).length
+
+  return sumList(
+    powList(exponent)(digits(number))
+  ) === number
 }
