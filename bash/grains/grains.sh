be6f5db565b0e0e817221d7b789d61e18c870042
Ivan Alcalde <mail.ivan.ag@gmail.com>
2022-09-28 15:24:58 +0200

    :white_check_mark: Bash - grains - iteration 1

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# grains = 2^(n-1)
+
+# we use the `bc` command because for some operations
+# with the typical arithmetic operators in bash
+# we are overflowing the max limit and the results
+# are in these cases wrong
+
+function get_grains() {
+  exponent="$(($1 - 1))"
+
+  echo "2^$exponent" | bc
+}
+
+function main() {
+  min=1
+  max=64
+
+  if (("$1" >= "$min" && "$1" <= "$max")); then
+    result=$(get_grains "$1")
+  elif [[ "$1" == "total" ]]; then
+    result=0
+    for ((i = "$min"; i <= "$max"; i++)); do
+      grains_of_i=$(get_grains "$i")
+      result=$(bc <<<"$result + $grains_of_i")
+    done
+  else
+    echo "Error: invalid input"
+    return 1
+  fi
+
+  echo "$result"
+}
+
+main "$@"
