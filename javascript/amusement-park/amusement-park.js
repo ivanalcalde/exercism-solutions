55f89ed6acbb401dae598c2a1b22e855f42d12ae
Ivan Alcalde <mail.ivan.ag@gmail.com>
2023-03-11 19:46:10 +0100

    javascript: amusement-park done

---
+++ javascript/amusement-park/amusement-park.js
@@ -0,0 +1,75 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  return { name, age, ticketId };
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+  if (visitor.ticketId) visitor.ticketId = null
+
+  return visitor
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+  const name = _getTicketName(tickets, ticketId)
+
+  if(name === null) return 'not sold'
+
+  if(name === undefined) return 'unknown ticket id'
+
+  return `sold to ${name}`
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+  const status = ticketStatus(tickets, ticketId)
+
+  if (status === 'not sold' || status === 'unknown ticket id') {
+    return 'invalid ticket !!!'
+  }
+
+  return _getTicketName(tickets, ticketId)
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+export function gtcVersion(visitor) {
+  return visitor.gtc?.version
+}
+
+function _getTicketName(tickets, ticketId) {
+  return tickets[ticketId]
+}
